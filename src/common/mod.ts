8e4d73bb08563ce0e72fc1683637d63e23ad14ae
Bryan Killian <bryan@trailmix.me>
2021-04-01 15:47:57 -0400

    one more capital

--- src/common/mod.ts
+++ src/common/mod.ts
@@ -2,7 +2,7 @@ export { unique } from "trailmix/common/array.ts";
 
 export { importDefault, validPath } from "trailmix/common/file.ts";
 
-// @deno-types="./File.d.ts"
+// @deno-types="./file.d.ts"
 export type { FileExtension, ImportOptions } from "trailmix/common/file.d.ts";
 
 export { resetTable, testFunction } from "trailmix/common/table.ts";
